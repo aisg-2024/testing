618e167dc8f9b5c761044bb1209c35dcaef675c6
yeo-menghan <yeomenghan1989@gmail.com>
2024-05-06 10:37:29 +0700

    add research.js, researchprompts from research paper and email_content folder

---
+++ email_content/fraud_email.js
@@ -0,0 +1,9 @@
+const fraud_email = `
+"Sender: ""Webmail Support monkey.org""   <notifications-email03.cpsrv2@eifka.net>
+Receiver: jose@monkey.org
+Date: 17 Nov 2022 00:05:31 -0800
+Subject: [CASE  ID: KTF-178-OxEoHbxIoE]:IMPORTANT: You have  1 new message(s) in your jose@monkey.org Mailbox
+Body: WebmailÂ  Hi jose,Â Â  Your e-Mail accountÂ Â jose@monkey.orgÂ  orÂ  infoÂ will be disconnected from sending or receiving mail from other usersÂ Â because you failed to resolve errors on your mail. You have 24 hours to retrieve your accountÂ or your account will be suspended.Kindly follow the instructions below to retrieve your Email now.Â  RETRIEVE ACCOUNT NOW Regards,jose@monkey.orgÂ Â Team CopyrightÂ Â  Â© 2022"≈
+  `;
+
+module.exports = fraud_email;
